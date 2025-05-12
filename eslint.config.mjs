c9d962fa977fee7431d3905f7983ceb537a4772f
RillSvai <kirilkvas07@gmail.com>
2025-05-12 11:44:53 +0300

    chore(eslint): Update ignore list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,13 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'dist/**', 'node_modules/**', 'coverage/**'],
+    ignores: [
+      'eslint.config.mjs',
+      'dist/**',
+      'node_modules/**',
+      'coverage/**',
+      'commitlint.config.cjs',
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
